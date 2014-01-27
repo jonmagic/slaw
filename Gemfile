7cb288a35f52bb6170335cecd09a3511b15b52dd
Jonathan Hoyt <hoyt@github.com>
2014-01-26 23:21:39 -0800

    Parse first ingredient expression

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "https://rubygems.org"
 
 gem "guard-minitest"
+gem "parslet"
 gem "rake"
--- spec/slaw_spec.rb
+++ spec/slaw_spec.rb
@@ -3,6 +3,13 @@ require "minitest/autorun"
 
 describe Slaw do
   it "has a version" do
-    assert_equal Slaw::VERSION, "0.0.1"
+    assert_equal "0.0.1", Slaw::VERSION
+  end
+
+  it "parses expression" do
+    slaw = Slaw.new.parse("1 cup flour")
+    assert_equal 1,       slaw[:quantity].to_i
+    assert_equal "cup",   slaw[:unit].to_s
+    assert_equal "flour", slaw[:ingredient].to_s
   end
 end
