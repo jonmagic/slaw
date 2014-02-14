4221f319c40d870b6e69814ce1724e77bf99886c
Jonathan Hoyt <hoyt@github.com>
2014-02-14 10:52:14 -0800

    Add support for fractional measures and optional units

--- lib/slaw/parser.rb
+++ lib/slaw/parser.rb
@@ -7,9 +7,16 @@ class Slaw
       eval Units.all.map {|r| "str('#{r}')"}.join(' | ')
     end
 
-    rule(:quantity)   { match("[0-9]").repeat(1) }
+    rule(:quantity)   { fraction | integer }
+    rule(:fraction)   { integer.as(:north) >> divider >> integer.as(:south) }
+    rule(:integer)    { match("[0-9]").repeat(1) }
+    rule(:divider)    { str("/") }
+
     rule(:unit)       { units }
-    rule(:ingredient) { match("[a-z]").repeat(1) }
+    rule(:unit?)      { units.maybe }
+
+    rule(:ingredient) { ingredient_part.repeat(1) }
+    rule(:ingredient_part) { match("[a-zA-Z]").repeat(1) >> comma? >> space? }
 
     rule(:space)      { match("\s").repeat(1) }
     rule(:space?)     { space.maybe }
@@ -17,9 +24,12 @@ class Slaw
     rule(:newline)    { match("\n").repeat(1) }
     rule(:newline?)   { newline.maybe }
 
+    rule(:comma)    { str(",") }
+    rule(:comma?)   { comma.maybe }
+
     rule(:instruction) {
       quantity.as(:quantity) >> space? \
-        >> unit.as(:unit) >> space? \
+        >> unit?.as(:unit) >> space? \
         >> ingredient.as(:ingredient) \
         >> newline?
     }
--- spec/lib/slaw/parser_spec.rb
+++ spec/lib/slaw/parser_spec.rb
@@ -1,15 +1,7 @@
 require "spec_helper"
 
 describe Slaw::Parser do
-  it "parses ingredient" do
-    ingredients = Slaw::Parser.new.parse("1 cup flour")
-    flour = ingredients.first
-    assert_equal 1,       flour[:quantity].to_i
-    assert_equal "cup",   flour[:unit].to_s
-    assert_equal "flour", flour[:ingredient].to_s
-  end
-
-  it "parses ingredient list" do
+  it "parses list of ingredients" do
     ingredients = Slaw::Parser.new.parse("1 cup flour\n2 tbsp butter")
     flour = ingredients[0]
     assert_equal 1,        flour[:quantity].to_i
@@ -19,5 +11,19 @@ describe Slaw::Parser do
     assert_equal 2,        butter[:quantity].to_i
     assert_equal "tbsp",   butter[:unit].to_s
     assert_equal "butter", butter[:ingredient].to_s
+  end
+
+  it "parses ingredient without unit" do
+    ingredients = Slaw::Parser.new.parse("4 boneless, skinless, chicken breasts")
+    assert_equal 4, ingredients[0][:quantity].to_i
+    assert_equal "boneless, skinless, chicken breasts", ingredients[0][:ingredient].to_s
+  end
+
+  it "parses fractional units" do
+    ingredients = Slaw::Parser.new.parse("3/4 c. chopped onion")
+    assert_equal 3, ingredients[0][:quantity][:north].to_i
+    assert_equal 4, ingredients[0][:quantity][:south].to_i
+    assert_equal "c.", ingredients[0][:unit].to_s
+    assert_equal "chopped onion", ingredients[0][:ingredient].to_s
   end
 end
